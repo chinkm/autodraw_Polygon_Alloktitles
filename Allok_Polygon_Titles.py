aa59c8931d1d445088f16eb5ae46033be7fd6787
Chin KM <ckm.tutorial@gmail.com>
2023-09-21 16:23:59 +0800

    Draw Polygon to canvas and add attribute table with expression

--- Allok_Polygon_Titles.py
+++ Allok_Polygon_Titles.py
@@ -1,4 +1,5 @@
 import csv
+import datetime
 from qgis.core import QgsVectorLayer, QgsField, QgsFeature, QgsGeometry, QgsProject
 from PyQt5.QtCore import QVariant
 
@@ -9,15 +10,15 @@ new_result=[]
 for i in result:
     new_result.append(list(filter(None, i)))
 
-#Read Land Titles' details 
+#Read Land Titles' details based on land titles
 with open("I:\Allok_Geopackage_Project\GIS_Allok_Titles_Project\Land Title QGIS\Group's Land Details.csv", encoding="utf-8-sig",newline="") as data1:
-    result1=list(csv.reader(data, delimiter=","))
+    result1=list(csv.reader(data1, delimiter=","))
 new_result1=[]
 for j in result1:
     new_result1.append(list(filter(None, j)))
 
 
-field_names=["fid","Title No","Title Type","Terms","Title Duration","Title Surveyed Area"] 
+field_names=["Title No","Title Type","Terms","Title Duration","Title Surveyed Area"] 
 layer = QgsVectorLayer("Polygon?crs=epsg:4326", "Allok_Polygon_Titles", "memory")
 layer.isValid()
 
@@ -25,8 +26,11 @@ fields=[]
 fields.append(QgsField("Title No", QVariant.String))
 fields.append(QgsField("Title Type", QVariant.String))
 fields.append(QgsField("Terms", QVariant.Int))
-fields.append(QgsField("Title Duration", QVariant.DateTime))
+fields.append(QgsField("Title Expiry", QVariant.String))
+fields.append(QgsField("Title Duration", QVariant.Int))
 fields.append(QgsField("Title Surveyed Area", QVariant.Double))
+fields.append(QgsField("GIS Surveyed Area", QVariant.Double))
+fields.append(QgsField("Variance Area", QVariant.Double))
 
 layer.startEditing()
 layer.dataProvider().addAttributes(fields)
@@ -35,19 +39,44 @@ layer.updateFields()
 polygon=[]
 geometries=[]
 
+#Create point to draw polygon based on new_result
 for row in new_result:
     for i in range(0, len(row)-1,2):
         polygon.append(QgsPointXY(float(row[i]),float(row[i+1])))
     geometries.append(QgsGeometry.fromPolygonXY([polygon]))
         
     polygon.clear()
-    
-    
+
+#Draw polygon to canvasmap
 for i, geometry in enumerate(geometries):
     feature=QgsFeature()
     feature.setGeometry(geometry)
     layer.dataProvider().addFeatures([feature])
 
+#Add table attributes with values based on new_result1
+n=1   
+for row1 in layer.getFeatures():
+    row1['Title No']=new_result1[n][0]
+    row1['Title Type']=new_result1[n][1]
+    row1['Terms']=new_result1[n][2]
+    row1['Title Expiry']=new_result1[n][3]
+    row1['Title Surveyed Area']=new_result1[n][4]
+    
+    #Fill Other columns with expressions in attribute table
+    expression1= QgsExpression('round($area * 0.000247,2)')
+    expression2=QgsExpression('to_int(right("Title Expiry",4))- to_int(left(now(),4))')
+    expression3=QgsExpression('to_double("Title Surveyed Area")-to_double("GIS Surveyed Area")')
+    context=QgsExpressionContext()
+    context.appendScopes(QgsExpressionContextUtils.globalProjectLayerScopes(layer))
+    context.setFeature(row1)
+    row1['GIS Surveyed Area']=expression1.evaluate(context)
+    row1['Title Duration']=expression2.evaluate(context)
+    row1['Variance Area']=expression3.evaluate(context)
+    
+            
+    layer.updateFeature(row1)
+    n+=1
+    
 layer.commitChanges()
 
 QgsProject.instance().addMapLayer(layer)
